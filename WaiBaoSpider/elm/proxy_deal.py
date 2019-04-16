dfaa2eb11d9e8397197c5aab23db39d87e42124e
James <hotjameschang@qq.com>
2019-04-16 18:33:04 +0800

    dxsds

--- WaiBaoSpider/elm/proxy_deal.py
+++ WaiBaoSpider/elm/proxy_deal.py
@@ -8,24 +8,22 @@ import json
 import redis
 from mitmproxy import ctx
 
-rediser = redis.Redis(host='127.0.0.1', port='6379', db=1, )
-
-detail = [1, "shanghaiaddr"]
-
+rediser = redis.Redis(host='127.0.0.1', port='6379', db=2, )
 
 def response(flow):
     ctx.log.info('>>>>>>>>>>><<<<<<<<<<<<<<<<<')
     url = flow.request.url
+    # 网易新闻
     if 'www.ele.me/restapi/v2/pois' in url:
         ctx.log.info('>>>>>>>>>>><<<<<<<<<<<<<<<<<')
         content = flow.response.content
         content_dict = json.loads(content)
         for info in content_dict:
             dl = []
-            if info["city_id"] == detail[0]:
+            if info["city_id"] == 1:
                 dl.append(str(info["count"]))
                 dl.append(info["geohash"])
                 dl.append(str(info["longitude"]))
                 dl.append(str(info["latitude"]))
                 str_add = "#$#$".join(dl)
-                rediser.sadd(detail[1], str_add)
+                rediser.sadd("shanghaiaddr", str_add)
